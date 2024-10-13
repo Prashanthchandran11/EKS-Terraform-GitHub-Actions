ddf48a5ff77c52ea76d69acdc0e85b45c19456bc
Prashanthchandran11 <prashanthchandran1198@gmail.com>
2024-10-13 20:33:49 +0530

    Update backend.tf
    r

--- eks/backend.tf
+++ eks/backend.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket         = "masterpower1507"
     region         = "us-east-1"
-    key            = "eks/terraform.tfstate"
+    key            = "EKS-Terraform-GitHub-Actions/eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
